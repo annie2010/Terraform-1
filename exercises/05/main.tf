63eaa69a7c2b692a461ceb887b04492ae024d89a
Develop Intelligence <marketing@developintelligence.com>
2020-07-16 21:14:23 -0600

    Update main.tf

--- exercises/05/main.tf
+++ exercises/05/main.tf
@@ -10,6 +10,5 @@ provider "aws" {
 # declare a resource stanza so we can create something.
 resource "aws_s3_bucket" "student_bucket_alt" {
   bucket   = "devint-${var.student_alias}-alt"
-  provider = aws.alternate
 }
 
